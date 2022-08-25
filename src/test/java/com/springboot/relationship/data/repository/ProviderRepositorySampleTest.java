04173b8487306e94986d1dcefd68de2b733aa877
JoeSangHyun <tiamet133@naver.com>
2022-08-25 23:28:33 +0900

    2022-08-25 Commit

--- src/test/java/com/springboot/relationship/data/repository/ProviderRepositorySampleTest.java
+++ src/test/java/com/springboot/relationship/data/repository/ProviderRepositorySampleTest.java
@@ -2,10 +2,12 @@ package com.springboot.relationship.data.repository;
 
 import com.springboot.relationship.data.entity.Product;
 import com.springboot.relationship.data.entity.Provider;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @SpringBootTest
@@ -79,4 +81,65 @@ public class ProviderRepositorySampleTest {
                 .orElseThrow(RuntimeException::new).getProvider());
     }
 
+    @Test
+    void cascadeTest() {
+        Provider provider = savedProvider("새로운 공급업체");
+
+        Product product1 = savedProduct("상품1",1000,1000);
+        Product product2 = savedProduct("상품1",500,1500);
+        Product product3 = savedProduct("상품1",750,500);
+
+        // 연관관계 설정
+        product1.setProvider(provider);
+        product2.setProvider(provider);
+        product3.setProvider(provider);
+
+        provider.getProductList().addAll(Lists.newArrayList(product1,product2,product3));
+
+        providerRepository.save(provider);
+    }
+
+    private Provider savedProvider(String name) {
+        Provider provider = new Provider();
+        provider.setName(name);
+
+        return provider;
+    }
+
+    private Product savedProduct(String name, Integer price, Integer stock) {
+        Product product = new Product();
+        product.setName(name);
+        product.setPrice(price);
+        product.setStock(stock);
+
+        return product;
+    }
+
+    @Test
+    @Transactional
+    void orphanRemovalTest() {
+        Provider provider = savedProvider("신규 업체");
+
+        Product product1 = savedProduct("상품1",1000,1000);
+        Product product2 = savedProduct("상품1",500,1500);
+        Product product3 = savedProduct("상품1",750,500);
+
+        // 연관관계 설정
+        product1.setProvider(provider);
+        product2.setProvider(provider);
+        product3.setProvider(provider);
+
+        provider.getProductList().addAll(Lists.newArrayList(product1,product2,product3));
+
+        providerRepository.saveAndFlush(provider);
+
+        providerRepository.findAll().forEach(System.out::println);
+        productRepository.findAll().forEach(System.out::println);
+
+        Provider foundProvider = providerRepository.findById(1L).get();
+        foundProvider.getProductList().remove(0);
+
+        providerRepository.findAll().forEach(System.out::println);
+        productRepository.findAll().forEach(System.out::println);
+    }
 }
